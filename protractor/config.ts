bf6d205974e558c9c7555d2af0a0f45360c14a82
Sebastian Villegas Zapata <sebasvilz@gmail.com>
2017-09-28 21:22:16 -0500

    Migration to typescript

---
+++ protractor/config.ts
@@ -0,0 +1,13 @@
+import { browser, Config } from 'protractor';
+
+export let config: Config = {
+    framework: 'jasmine',
+    seleniumAddress: 'http://localhost:4444/wd/hub',
+    specs: [
+        '../test/Google.spec.js'
+    ],
+    noGlobals: true,
+    onPrepare: function () {
+        browser.ignoreSynchronization = true;
+    }
+};
