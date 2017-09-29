85b3d24b5f8477bc434483298b334141d203899d
Sebastian Villegas Zapata <sebasvilz@gmail.com>
2017-09-28 21:43:34 -0500

    Removed: Promise manager

--- protractor/config.ts
+++ protractor/config.ts
@@ -1,13 +1,15 @@
 import { browser, Config } from 'protractor';
+import { reporter } from './helper/reporter';
 
 export let config: Config = {
     framework: 'jasmine',
-    seleniumAddress: 'http://localhost:4444/wd/hub',
     specs: [
         '../test/Google.spec.js'
     ],
     noGlobals: true,
     onPrepare: function () {
         browser.ignoreSynchronization = true;
-    }
+        reporter();
+    },
+    SELENIUM_PROMISE_MANAGER: false,
 };
