4ae0b72dce34e0e204d233977234f63ded0e0db4
svillegasz <30663053+svillegasz@users.noreply.github.com>
2017-10-04 14:18:21 -0500

    Promise manager (#5)
    * setup mocha configuration
    
    * Refactor: Google spec
    
    * Migration to typescript
    
    * Removed: Promise manager

--- protractor/config.ts
+++ protractor/config.ts
@@ -1,9 +1,8 @@
 import { browser, Config } from 'protractor';
-import { reporter }   from './helper/reporter';
+import { reporter } from './helper/reporter';
 
 export let config: Config = {
     framework: 'jasmine',
-    seleniumAddress: 'http://localhost:4444/wd/hub',
     specs: [
         '../test/Google.spec.js'
     ],
@@ -11,5 +10,6 @@ export let config: Config = {
     onPrepare: function () {
         browser.ignoreSynchronization = true;
         reporter();
-    }
+    },
+    SELENIUM_PROMISE_MANAGER: false,
 };
