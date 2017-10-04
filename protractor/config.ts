31f866b5d0157e143c2fc7d45e58e5e158576b1c
svillegasz <30663053+svillegasz@users.noreply.github.com>
2017-10-04 14:05:04 -0500

    Reporter (#4)
    * setup mocha configuration
    
    * Refactor: Google spec
    
    * Migration to typescript
    
    * Added: console reporter
    
    * Deleted: White spaces

--- protractor/config.ts
+++ protractor/config.ts
@@ -1,4 +1,5 @@
 import { browser, Config } from 'protractor';
+import { reporter }   from './helper/reporter';
 
 export let config: Config = {
     framework: 'jasmine',
@@ -9,5 +10,6 @@ export let config: Config = {
     noGlobals: true,
     onPrepare: function () {
         browser.ignoreSynchronization = true;
+        reporter();
     }
 };
