55f4735e56370a5df9244a7f8bfeec6a04c4692f
svillegasz <svillegaz@psl.com.co>
2017-10-05 13:01:10 -0500

    Fixed: EOF. Linter rules

--- protractor/headless.config.ts
+++ protractor/headless.config.ts
@@ -2,20 +2,20 @@ import { browser, Config } from 'protractor';
 import { reporter } from './helper/reporter';
 
 export let config: Config = {
-    framework: 'jasmine',
-    specs: [
-        '../test/Google.spec.js'
-    ],
-    capabilities: {
-        browserName: 'chrome',
-        chromeOptions: {
-            args: ['--headless', '--disable-gpu', '--window-size=800,600']
-        }
-    },
-    noGlobals: true,
-    onPrepare: function () {
-        browser.ignoreSynchronization = true;
-        reporter();
-    },
-    SELENIUM_PROMISE_MANAGER: false,
+  framework: 'jasmine',
+  specs: [
+    '../test/Google.spec.js'
+  ],
+  capabilities: {
+    browserName: 'chrome',
+    chromeOptions: {
+      args: ['--headless', '--disable-gpu', '--window-size=800,600']
+    }
+  },
+  noGlobals: true,
+  onPrepare() {
+    browser.ignoreSynchronization = true;
+    reporter();
+  },
+  SELENIUM_PROMISE_MANAGER: false,
 };
