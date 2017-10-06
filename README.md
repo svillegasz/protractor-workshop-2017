48c1c4e28ff907f5ae7b361bda5bc770737aea3a
svillegasz <30663053+svillegasz@users.noreply.github.com>
2017-10-06 08:44:09 -0500

    Linter (#8)
    * setup mocha configuration
    
    * Refactor: Google spec
    
    * Migration to typescript
    
    * Removed: Promise manager
    
    * Added: Chrome headless config
    
    * Added: Travis CI
    
    * Added: Linter
    
    * Travis status img fixed
    
    * Fixed: EOF. Linter rules

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
-[![Build Status](https://travis-ci.org/svillegasz/psl-workshop-protractor.svg?branch=master)]
+[![Build Status](https://travis-ci.org/svillegasz/psl-workshop-api-testing-js.svg?branch=master)](https://travis-ci.org/svillegasz/psl-workshop-api-testing-js)
+
 Protractor - PSL Workshop - 2017
--- protractor/config.ts
+++ protractor/config.ts
@@ -2,14 +2,14 @@ import { browser, Config } from 'protractor';
 import { reporter } from './helper/reporter';
 
 export let config: Config = {
-    framework: 'jasmine',
-    specs: [
-        '../test/Google.spec.js'
-    ],
-    noGlobals: true,
-    onPrepare: function () {
-        browser.ignoreSynchronization = true;
-        reporter();
-    },
-    SELENIUM_PROMISE_MANAGER: false,
+  framework: 'jasmine',
+  specs: [
+    '../test/Google.spec.js'
+  ],
+  noGlobals: true,
+  onPrepare() {
+    browser.ignoreSynchronization = true;
+    reporter();
+  },
+  SELENIUM_PROMISE_MANAGER: false,
 };
