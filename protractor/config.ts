0ee7d7710a50651827522be6fdd37d3dae6d417a
svillegasz <30663053+svillegasz@users.noreply.github.com>
2017-10-06 10:20:55 -0500

    Css selector (#9)
    * setup mocha configuration
    
    * Refactor: Google spec
    
    * Migration to typescript
    
    * Removed: Promise manager
    
    * Added: Chrome headless config
    
    * Added: Travis CI
    
    * Added: Linter
    
    * Removed: Google specs. Added: Buy Tshirt Specs
    
    * Google spec deleted
    
    * Fixed: Linter rules
    
    * Fixed: CSS selector

--- protractor/config.ts
+++ protractor/config.ts
@@ -3,11 +3,10 @@ import { reporter } from './helper/reporter';
 
 export let config: Config = {
   framework: 'jasmine',
-  specs: [
-    '../test/Google.spec.js'
-  ],
-  noGlobals: true,
-  onPrepare() {
+  specs: ['../test/**/*.spec.js'],
+  noGlobals: true,    
+  getPageTimeout: 1000,
+  onPrepare () {
     browser.ignoreSynchronization = true;
     reporter();
   },
