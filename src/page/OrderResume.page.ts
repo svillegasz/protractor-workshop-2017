21da0f740b110e31267b2ac2daf5d4376df0951c
svillegasz <svillegaz@psl.com.co>
2017-10-09 09:51:46 -0500

    Linter rules fixed

--- src/page/OrderResume.page.ts
+++ src/page/OrderResume.page.ts
@@ -1,11 +1,11 @@
 import { $, ElementFinder, promise } from 'protractor';
 
 export class OrderResumePage {
- public get completeMessage(): ElementFinder {
-   return $('#center_column > div > p > strong');
- }
+  public get completeMessage(): ElementFinder {
+    return $('#center_column > div > p > strong');
+  }
 
- public getCompleteMessage(): promise.Promise<String> {
-   return this.completeMessage.getText();
- }
+  public getCompleteMessage(): promise.Promise<String> {
+    return this.completeMessage.getText();
+  }
 }
--- src/page/ProductDetail.page.ts
+++ src/page/ProductDetail.page.ts
@@ -1,12 +1,11 @@
 import { $, ElementFinder, promise } from 'protractor';
 
 export class ProductDetailPage {
- private get addButton(): ElementFinder {
-   return $('#add_to_cart > button > span');
- }
+  private get addButton(): ElementFinder {
+    return $('#add_to_cart > button > span');
+  }
 
- public addToCart(): promise.Promise<void> {
-   return this.addButton.click();
- }
+  public addToCart(): promise.Promise<void> {
+    return this.addButton.click();
+  }
 }
-  
