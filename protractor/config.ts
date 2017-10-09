fc972293d5fa600669b4fdfdbdc3d9bde239d860
svillegasz <30663053+svillegasz@users.noreply.github.com>
2017-10-09 09:48:24 -0500

    Page timeout (#11)
    * setup mocha configuration
    
    * Refactor: Google spec
    
    * Migration to typescript
    
    * Removed: Promise manager
    
    * Added: Chrome headless config
    
    * Added: Travis CI
    
    * Added: Linter
    
    * Removed: Google specs. Added: Buy Tshirt Specs
    
    * Added: Page object pattern
    
    * Added: Page objects
    
    * Added: Page timeout on config
    
    * Linter rules fixed

--- protractor/config.ts
+++ protractor/config.ts
@@ -2,13 +2,16 @@ import { browser, Config } from 'protractor';
 import { reporter } from './helper/reporter';
 
 export let config: Config = {
-  framework: 'jasmine',
-  specs: ['../test/**/*.spec.js'],
-  noGlobals: true,    
-  getPageTimeout: 1000,
-  onPrepare () {
-    browser.ignoreSynchronization = true;
-    reporter();
-  },
-  SELENIUM_PROMISE_MANAGER: false,
+    framework: 'jasmine',
+    specs: ['../test/**/*.spec.js'],
+    noGlobals: true,
+    getPageTimeout: 30000,
+    jasmineNodeOpts: {
+        defaultTimeoutInterval: 120000
+    },
+    onPrepare: function () {
+        browser.ignoreSynchronization = true;
+        reporter();
+    },
+    SELENIUM_PROMISE_MANAGER: false,
 };
--- protractor/headless.config.ts
+++ protractor/headless.config.ts
@@ -2,19 +2,22 @@ import { browser, Config } from 'protractor';
 import { reporter } from './helper/reporter';
 
 export let config: Config = {
-  framework: 'jasmine',
-  specs: ['../test/**/*.spec.js'],
-  capabilities: {
-    browserName: 'chrome',
-    chromeOptions: {
-      args: ['--headless', '--disable-gpu', '--window-size=800,600']
-    }
-  },
-  getPageTimeout: 1000,
-  noGlobals: true,
-  onPrepare() {
-    browser.ignoreSynchronization = true;
-    reporter();
-  },
-  SELENIUM_PROMISE_MANAGER: false,
+    framework: 'jasmine',
+    specs: ['../test/**/*.spec.js'],
+    capabilities: {
+        browserName: 'chrome',
+        chromeOptions: {
+            args: ['--headless', '--disable-gpu', '--window-size=800,600']
+        }
+    },
+    getPageTimeout: 30000,
+    jasmineNodeOpts: {
+        defaultTimeoutInterval: 120000
+    },    
+    noGlobals: true,
+    onPrepare: function () {
+        browser.ignoreSynchronization = true;
+        reporter();
+    },
+    SELENIUM_PROMISE_MANAGER: false,
 };
