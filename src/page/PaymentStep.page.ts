68bf98faa82a30a08146ace3a7f061e70ca4d5e2
svillegasz <svillegaz@psl.com.co>
2017-10-10 12:06:45 -0500

    removed anchor

--- src/page/PaymentStep.page.ts
+++ src/page/PaymentStep.page.ts
@@ -2,7 +2,7 @@ import { $, ElementFinder, promise } from 'protractor';
 
 export class PaymentStepPage {
   private get bankPaymentButton(): ElementFinder {
-    return $('p.payment_module > a.bankwire');
+    return $('p.payment_module > .bankwire');
   }
 
   public payByBank(): promise.Promise<void> {
