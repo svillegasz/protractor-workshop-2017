c1dcbca18c29f7b2f23cc6c7cca1ef162b8acec3
svillegasz <svillegaz@psl.com.co>
2017-10-10 08:47:11 -0500

    Final remove: unused await

--- src/page/BankPayment.page.ts
+++ src/page/BankPayment.page.ts
@@ -1,14 +1,11 @@
-import { $, ElementFinder, ExpectedConditions, browser } from 'protractor';
+import { $, ElementFinder, promise } from 'protractor';
 
 export class BankPaymentPage {
-  private until = ExpectedConditions;
-
   private get confirmButton(): ElementFinder {
     return $('#cart_navigation > button > span');
   }
 
-  public async confirmOrder(): Promise<void> {
-    await browser.wait(this.until.presenceOf(this.confirmButton), 3000);
-    await this.confirmButton.click();
+  public confirmOrder(): promise.Promise<void> {
+    return this.confirmButton.click();
   }
 }
