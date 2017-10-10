32433a2d0aca379bc9102e762864fe0da1e19380
svillegasz <svillegaz@psl.com.co>
2017-10-10 15:39:59 -0500

    Chaining and filtering elements

--- src/page/ProductList.page.ts
+++ src/page/ProductList.page.ts
@@ -1,11 +1,21 @@
-import { $, ElementFinder, promise } from 'protractor';
+import { $$, ElementFinder, ElementArrayFinder, promise } from 'protractor';
 
 export class ProductListPage {
-  private get productItem(): ElementFinder {
-    return $('a.product_img_link > img');
+  private get products(): ElementArrayFinder {
+    return $$('.product-container');
   }
 
-  public goToProductItemDetails(): promise.Promise<void> {
-    return this.productItem.click();
+  private findByProduct(name: String): ElementFinder{
+    return this.products
+      .filter((product) => product.$('.product-name').getText()
+        .then(text => text === name)).first();
+  }
+
+  private selectProduct(name: String): promise.Promise<void>{
+    return this.findByProduct(name).click();
+  }
+
+  public goToProductItemDetails(name: String): promise.Promise<void> {
+    return this.selectProduct(name);
   }
 }
