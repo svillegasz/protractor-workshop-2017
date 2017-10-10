e246e5f36e0859149212281304c0a80cdaca39b6
svillegasz <30663053+svillegasz@users.noreply.github.com>
2017-10-10 14:10:19 -0500

    Updated: element locators (#14)
    * Updated: element locators
    
    * removed anchor

--- src/page/BankPayment.page.ts
+++ src/page/BankPayment.page.ts
@@ -2,7 +2,7 @@ import { $, ElementFinder, promise } from 'protractor';
 
 export class BankPaymentPage {
   private get confirmButton(): ElementFinder {
-    return $('#cart_navigation > button > span');
+    return $('#cart_navigation > button[type="submit"]');
   }
 
   public confirmOrder(): promise.Promise<void> {
--- src/page/ProductList.page.ts
+++ src/page/ProductList.page.ts
@@ -2,7 +2,7 @@ import { $, ElementFinder, promise } from 'protractor';
 
 export class ProductListPage {
   private get productItem(): ElementFinder {
-    return $('#center_column > ul > li > div > div.left-block > div > a.product_img_link > img');
+    return $('a.product_img_link > img');
   }
 
   public goToProductItemDetails(): promise.Promise<void> {
