751d07673413ba95e4da20b205644a31e47f985e
svillegasz <30663053+svillegasz@users.noreply.github.com>
2017-10-11 13:53:47 -0500

    New locators (#19)
    * Spect refacotr: describes added
    
    * whens removed
    
    * added: disable-infobars capability
    
    * Chaining and filtering elements
    
    * Personal information created
    
    * Page object
    
    * Spec: fill form and submit
    
    * Fixed: linter rules
    
    * Deleted reports
    
    * Refactor: Personal Information page
    
    * Run all specs
    
    * Linter rules fixed
    
    * Added: getter methods

---
+++ src/page/PersonalInformation.page.ts
@@ -0,0 +1,93 @@
+import { $, ElementFinder, promise, browser, by } from 'protractor';
+
+export class PersonalInformationPage {
+  private get title(): ElementFinder {
+    return $('.page-title h1');
+  }
+
+  private get firstName(): ElementFinder {
+    return $('input[name="firstname"]');
+  }
+
+  private get lastName(): ElementFinder {
+    return $('input[name="lastname"]');
+  }
+
+  private get continent(): ElementFinder {
+    return $('#continents');
+  }
+
+  private get seleniumCommands(): ElementFinder {
+    return $('#selenium_commands');
+  }
+
+  private get submit(): ElementFinder {
+    return $('#submit');
+  }
+
+  private getSex(sexName: String): ElementFinder {
+    return $(`input[name="sex"][value="${sexName}"]`);
+  }
+  
+  private getYearsOfExperience(years: number): ElementFinder {
+    return $(`input[name="exp"][value="${years}"]`);
+  }
+
+  private getProfession(profession: String): ElementFinder {
+    return $(`input[name="profession"][value="${profession}"]`);
+  }
+
+  private getTool(tool: String): ElementFinder {
+    return $(`input[name="tool"][value="${tool}"]`);
+  }
+
+  private selectSex(sexName: String): promise.Promise<void> {
+    return this.getSex(sexName).click();
+  }
+
+  private selectExperience(years: number): promise.Promise<void> {
+    return this.getYearsOfExperience(years).click();
+  }
+
+  private async selectProfessions(professions: String[]): Promise<void> {
+    for (const profession of professions) {
+      await this.getProfession(profession).click();
+    }
+  }
+
+  private async selectTools(tools: String[]): Promise<void> {
+    for (const tool of tools) {
+      await this.getTool(tool).click();
+    }
+  }
+
+  private selectContinent(continent: string): promise.Promise<void> {
+    return this.continent.element(by.cssContainingText('option', continent)).click();
+  }
+
+  private async selectCommands(commands: string[]): Promise<void> {
+    for (const command of commands) {
+      await this.seleniumCommands.element(by.cssContainingText('option', command)).click();
+    }
+  }
+
+  public get(): promise.Promise<void> {
+    return browser.get('http://toolsqa.com/automation-practice-form/');
+  }
+
+  public async fillForm(formData: any): Promise<void> {
+    await this.firstName.sendKeys(formData.firstName);
+    await this.lastName.sendKeys(formData.lastName);
+    await this.selectSex(formData.sex);
+    await this.selectExperience(formData.experience);
+    await this.selectProfessions(formData.profession);
+    await this.selectTools(formData.tools);
+    await this.selectContinent(formData.continent);
+    await this.selectCommands(formData.commands);
+    await this.submit.click();
+  }
+
+  public getTitle(): promise.Promise<String> {
+    return this.title.getText();
+  }
+}
---
+++ test/Locators.spec.ts
@@ -0,0 +1,34 @@
+import { PersonalInformationPage } from '../src/page';
+
+describe('Given the tools QA page', () => {
+  const personalInformationPage: PersonalInformationPage = new PersonalInformationPage();
+
+  beforeEach(async () => {
+    await personalInformationPage.get();
+  });
+
+  describe('When I fill the form', () => {
+    beforeEach(async () => {
+      await personalInformationPage.fillForm({
+        firstName: 'Alejandro',
+        lastName: 'Perdomo',
+        sex: 'Male',
+        experience: 7,
+        profession: ['Automation Tester'],
+        tools: ['Selenium Webdriver'],
+        continent: 'South America',
+        commands: [
+          'Browser Commands',
+          'Navigation Commands',
+          'Switch Commands',
+          'Wait Commands',
+          'WebElement Commands']
+      });
+    });
+
+    it('Then it should display the correct title', () => {
+      expect(personalInformationPage.getTitle())
+        .toMatch('Automation Practice Form');
+    });
+  });
+});
